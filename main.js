b0277afc4eb6d1df4c979289f79519e63ec79c01
idohtml <Jacques.kanu@gmail.com>
2024-03-02 19:19:15 +0100

    updated animation with javascript

---
+++ main.js
@@ -0,0 +1,15 @@
+const buttonAnimation = document.querySelectorAll("button");
+
+buttonAnimation.forEach((button) => {
+  button.addEventListener("click", () => {
+    const currentState = button.getAttribute("data-state");
+
+    if (!currentState || currentState === "closed") {
+      button.setAttribute("data-state", "opened");
+      button.setAttribute("aria-expanded", "true");
+    } else {
+      button.setAttribute("data-state", "closed");
+      button.setAttribute("aria-expanded", "false");
+    }
+  });
+});
